2d58133c20a986badf95fea8269490bcaefc6f99
hezrom12 <hezrom12@gmail.com>
2014-05-30 01:28:29 -0300

    Associação V1

--- UCDiagram/src/UCDiagram/Associacao.java
+++ UCDiagram/src/UCDiagram/Associacao.java
@@ -18,12 +18,16 @@ public class Associacao extends Figura{
     
      public boolean intersecta(int x, int y) {
         if (x < posX) return false;
-        if (x > (posX + tam)) return false;
+        if (x > (posX + 250)) return false;
         if (y < posY) return false;
         if (y > (posY + tam)) return false;
         return true;
-     }
+    }
     public void desenha(Graphics g) {
-        g.drawLine(posX, posY, posY, posX);
+        g.drawLine(posX, posY, posX+250, posY);
+        if (this.estaSelecionado()) {
+        g.drawOval(posX-2,posY-2,4,4);
+        g.drawOval(posX+250-2,posY-2,4,4);
+        }
     }
 }
